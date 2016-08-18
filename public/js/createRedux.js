208db6afc074a0d2396d0f602de461ddfb7da494
zhangtiantian10 <975571814@qq.com>
2016-08-18 16:32:07 +0800

    show the todolist

---
+++ public/js/createRedux.js
@@ -0,0 +1,4 @@
+export default function reducer(state = {todos: [{text: '123', isDone: false},{text: '1233', isDone: false}]},action){
+
+    return state;
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,31 @@
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import reducer from './createRedux';
+
+const store = createStore(reducer);
 
 class Todolist extends Component {
     render() {
         return <div>
             <h1>todos</h1>
             <input type="text"/>
+            <Todos todos={store.getState().todos}/>
         </div>
     }
 }
 
-ReactDOM.render(<Todolist/>,document.getElementById('root'))
+class Todos extends React.Component {
+    render() {
+        return <div>
+            {this.props.todos.map(todo => {
+                return <div>
+                    {todo.text}
+                </div>
+            })}
+        </div>
+    }
+}
+
+
+ReactDOM.render(<Todolist/>, document.getElementById('root'));
