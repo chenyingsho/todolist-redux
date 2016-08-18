da7d906c8bdacde4c7efa7ebd4586447ed8e04b0
zhangtiantian10 <975571814@qq.com>
2016-08-18 18:35:28 +0800

    The end

--- public/js/createRedux.js
+++ public/js/createRedux.js
@@ -1,16 +1,23 @@
-export default function reducer(state = {todos: [],filterName:"ALL"},action){
-   switch(action.type) {
-       case 'ADD':
-           state.todos.push({text: action.text, isDone:false});
-           return state;
-       case 'TOGGLE':
-           state.todos[action.index].isDone = !state.todos[action.index].isDone;
-           console.log(state);
-           return state;
-       case 'FILTER':
-           state.filterName = action.filterName;
-           return state;
-       default:
-           return state;
-   }
+let id = 0;
+export default function reducer(state = {todos: [], filterName: "ALL"}, action) {
+    switch (action.type) {
+        case 'ADD':
+            state.todos.push({id, text: action.text, isDone: false});
+            id++;
+            console.log(id);
+            return state;
+        case 'TOGGLE':
+            let todo = state.todos.find(todo => todo.id === action.index);
+            todo.isDone = !todo.isDone;
+            return state;
+        case 'FILTER':
+            state.filterName = action.filterName;
+            return state;
+        case 'DELETE':
+            let Todo = state.todos.find(todo => todo.id === action.index);
+            state.todos.splice(state.todos.indexOf(Todo), 1);
+            return state;
+        default:
+            return state;
+    }
 }
--- public/js/index.js
+++ public/js/index.js
@@ -6,47 +6,55 @@ import reducer from './createRedux';
 const store = createStore(reducer);
 
 class Todolist extends Component {
-    add(text){
-        store.dispatch({type:'ADD',text})
+    add(text) {
+        store.dispatch({type: 'ADD', text})
     }
 
     toggle(index) {
-        store.dispatch({type:'TOGGLE', index})
+        store.dispatch({type: 'TOGGLE', index})
     }
 
-    show(filterName){
-        store.dispatch({type:"FILTER",filterName})
+    show(filterName) {
+        store.dispatch({type: "FILTER", filterName})
     }
 
     filterTodos() {
-        if(store.getState().filterName === 'ALL') {
+        if (store.getState().filterName === 'ALL') {
             return store.getState().todos;
-        } else if(store.getState().filterName === 'ACTIVE') {
+        } else if (store.getState().filterName === 'ACTIVE') {
             return store.getState().todos.filter(todo => !todo.isDone);
         } else {
             return store.getState().todos.filter(todo => todo.isDone);
         }
     }
 
-    render() {
-        return <div>
-            <h1>todos</h1>
-            <Add onAdd={this.add.bind(this)}/>
-            <Todos todos={this.filterTodos()} onToggle={this.toggle.bind(this)}/>
-            <Item onShow={this.show} />
-        </div>
-    }
-}
-class Item extends React.Component {
-    show(type){
-        this.props.onShow(type)
+    deleteTodo(index) {
+        store.dispatch({type: 'DELETE', index})
     }
 
     render() {
         return <div>
-            <span onClick={this.show.bind(this,'ALL')}>ALL</span>&nbsp;&nbsp;&nbsp;
-            <span onClick={this.show.bind(this,'ACTIVE')}>ACTIVE</span>&nbsp;&nbsp;&nbsp;
-            <span onClick={this.show.bind(this,'COMPLETE')}>COMPLETE</span>
+            <h1>todos</h1>
+            <Add onAdd={this.add.bind(this)}/>
+            <Todos todos={this.filterTodos()} onToggle={this.toggle.bind(this)} onDelete={this.deleteTodo.bind(this)}/>
+            <Item onShow={this.show} filterName={store.getState().filterName}/>
+        </div>
+    }
+}
+
+class Item extends React.Component {
+    show(type) {
+        this.props.onShow(type)
+    }
+
+    render() {
+        const links = ['ALL', 'ACTIVE', 'COMPLETED'].map(filterName => {
+            return <span style={{"fontSize": this.props.filterName === filterName ? "28px" : "16px"}}
+                         onClick={this.show.bind(this, filterName)}>{filterName}&nbsp;&nbsp;&nbsp;</span>;
+
+        });
+        return <div>
+            {links}
         </div>
     }
 }
@@ -56,12 +64,17 @@ class Todos extends React.Component {
         this.props.onToggle(index);
     }
 
+    deleteTodo(index) {
+        this.props.onDelete(index);
+    }
+
     render() {
         return <div>
-            {this.props.todos.map((todo,index) => {
+            {this.props.todos.map((todo, index) => {
                 return <div key={index}>
-                    <input type="checkbox" checked={todo.isDone} onChange={this.toggle.bind(this, index)}/>
+                    <input type="checkbox" checked={todo.isDone} onChange={this.toggle.bind(this, todo.id)}/>
                     <span style={{"textDecoration": todo.isDone ? "line-through" : ""}}>{todo.text}</span>
+                    <button onClick={this.deleteTodo.bind(this, todo.id)}>X</button>
                 </div>
             })}
         </div>
@@ -69,10 +82,12 @@ class Todos extends React.Component {
 }
 
 class Add extends React.Component {
-    add(){
-        const test=this.refs.add.value;
+    add() {
+        const test = this.refs.add.value;
         this.props.onAdd(test);
+        this.refs.add.value = '';
     }
+
     render() {
         return <div>
             <input type="text" ref="add"/>
@@ -81,17 +96,9 @@ class Add extends React.Component {
     }
 }
 
-class Delete extends React.Component {
-    render() {
-        return <div>
-
-        </div>
-    }
-}
-
 
 ReactDOM.render(<Todolist/>, document.getElementById('root'));
 
-store.subscribe(()=>{
+store.subscribe(()=> {
     ReactDOM.render(<Todolist/>, document.getElementById('root'));
 })
