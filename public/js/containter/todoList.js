2076e6fa66f30e8719e36768eb883af50b4343fe
2smallears <914197237@qq.com>
2016-08-20 11:57:34 +0800

    achieve delete

--- public/js/containter/todoList.js
+++ public/js/containter/todoList.js
@@ -2,22 +2,38 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 const TodoList = React.createClass({
-  render: function() {
-      const todo=this.props.todos.map((todo,index)=>{
-          return <div key={index}>
-              {todo.text}
-              <button>X</button>
-          </div>
-      });
-    return <div>
-        {todo}
-    </div>;
-  }
+    changeState(index){
+        this.props.changeState(index);
+    },
+    onDelete(index){
+        this.props.onDelete(index);
+    },
+    render: function () {
+        const todo = this.props.todos.map((todo, index)=> {
+            return <div key={index}>
+                <input type="checkbox" onClick={this.changeState.bind(this, index)}/>
+                {todo.text}
+                <button onClick={this.onDelete.bind(this, todo.id)}>X</button>
+            </div>
+        });
+        return <div>
+            {todo}
+        </div>;
+    }
 });
 
 
-function mapStateToProps(state){
-    return {todos:state.todos}
+function mapStateToProps(state) {
+    return {todos: state.todos}
 }
-
-export default connect(mapStateToProps)(TodoList);
+function mapDispatchToProps(dispatch) {
+    return {
+        changeState: (index)=> {
+            dispatch({type: 'CHANGE', index})
+        },
+        onDelete: (index)=> {
+            dispatch({type: 'DELETE', index})
+        }
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- public/js/reducers/app-reducer.js
+++ public/js/reducers/app-reducer.js
@@ -1,15 +1,34 @@
-export default function (state={todos:[]},action) {
-    switch(action.type){
+let id = 0;
+export default function (state = {todos: []}, action) {
+    switch (action.type) {
         case "ADD":
             return {
-                todos:[
+                todos: [
                     ...state.todos,
                     {
-                        text:action.text,
-                        isDone:false
+                        text: action.text,
+                        isDone: false,
+                        id: ++id
                     }
                 ]
             };
+
+        case 'CHANGE':
+            state.todos[action.index].isDone = !state.todos[action.index].isDone;
+            return {
+                todos: [
+                    ...state.todos
+                ]
+            }
+        case 'DELETE':
+            let index = state.todos.indexOf(state.todos.find(t=>t.id===action.index));
+            state.todos.splice(index,1);
+            return{
+                todos:[
+                    ...state.todos
+                ]
+            }
+
     }
     return state;
 }
